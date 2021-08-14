f1b5b152948be53157bf8fdd21a1de8e823be688
Luis Dufour <luis.dfour@gmail.com>
2021-08-14 17:37:16 -0300

    consumo de API provincias para select del form

--- js/main.js
+++ js/main.js
@@ -11,3 +11,33 @@ $navbarbutton.addEventListener('click', (e)=>{
     $menu.classList.toggle('nav__dropdown');
     $navbarIcon.classList.toggle('navbar__arrow');
 })
+
+
+//Form
+
+const $selectProv = document.getElementById('prov');
+
+const traerDatosProvincias = ()=>{
+    let xhr = new XMLHttpRequest();
+    
+
+    xhr.open('GET', 'https://apis.datos.gob.ar/georef/api/provincias', true);
+
+    xhr.addEventListener('load', (data)=>{
+        const provincias = JSON.parse(data.target.response).provincias;
+        const fragment = document.createDocumentFragment();
+
+        provincias.forEach(provincia => {
+            const optionProv = document.createElement('OPTION');
+            optionProv.textContent = provincia.nombre;
+
+            fragment.appendChild(optionProv);
+        });
+
+        $selectProv.appendChild(fragment);
+    })
+
+    xhr.send();
+}
+
+$selectProv.addEventListener('click', traerDatosProvincias());
